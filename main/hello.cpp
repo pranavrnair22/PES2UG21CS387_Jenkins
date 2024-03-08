701eddfbb822eab311c5429963c2cc6eafeaf4ef
pranavrnair22 <mail.pranavrnair@gmail.com>
2024-03-08 11:51:03 +0530

    commit changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run, making some changes\n";
+  cout << "I have successfully built and run, made some changes\n";
   return 0;
 }
